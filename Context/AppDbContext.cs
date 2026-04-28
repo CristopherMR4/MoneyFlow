f6009e2ec5ffdc5c6babe3c4bc6c11fe702a2408
Cristopher MR <cristophermr04@gmail.com>
2026-04-28 18:59:56 -0400

    feat: Creacion del AppDbCotext y DbEntindades, asi como la instalacion de paquetes para el EF CORE

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoneyFlow.Entities;
+
+namespace MoneyFlow.Context
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext>options):base(options)
+        {
+            
+        }
+
+        public DbSet<Users> Users { get; set; }
+        public DbSet<Services> Services { get; set; }
+        public DbSet<Transactions> Transactions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Users>(e =>
+            {
+                e.HasKey("UserID");
+                e.Property("UserID").ValueGeneratedOnAdd();
+
+                e.HasData(
+                  
+                    new Users() { UserID = 1, UserName = "Cristopher Mariñez" , Password="Cris123"}
+                    );
+                
+            });
+        }
+
+
+    }
+}
