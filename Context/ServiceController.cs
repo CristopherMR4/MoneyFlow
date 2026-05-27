4001526c8a9fb2950848c4b658326f98647e9920
Cristopher MR <cristophermr04@gmail.com>
2026-05-26 20:17:23 -0400

    feat: creacion del controlador y vista de Service

---
+++ Context/ServiceController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MoneyFlow.Context
+{
+    public class ServiceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
