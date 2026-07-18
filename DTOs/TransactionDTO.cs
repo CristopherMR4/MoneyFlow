0607370c85761bc6182a248dae15ae7cefc9a178
Cristopher MR <cristophermr04@gmail.com>
2026-07-18 18:56:39 -0400

    feat: Modelos/Datos  agregados DTO de transacciones

--- DTOs/TransactionDTO.cs
+++ DTOs/TransactionDTO.cs
@@ -1,7 +1,24 @@
-﻿namespace MoneyFlow.DTOs
+﻿using MoneyFlow.Entities;
+
+namespace MoneyFlow.DTOs
 {
     public class TransactionDTO
     {
+
+        //Funcion de llave foreanas.
+        public int Userid { get; set; }
+
+        public int Serviceid { get; set; }
+
+            //Funciones primarias para el DTOs
+        public string Comment { get; set; }
+
+        public DateOnly Date { get; set; }
+
+        public decimal TotalAmount { get; set; }
+
+
+
 
     }
 }
