02ab04a390e92d7b4499b54acff72a0e64fca6c3
Cristopher MR <cristophermr04@gmail.com>
2026-07-18 19:39:59 -0400

    feat: Creacion de Contenedor del DTO en TransactionManager

--- Managers/TransactionManager.cs
+++ Managers/TransactionManager.cs
@@ -1,6 +1,27 @@
-﻿namespace MoneyFlow.Managers
+﻿using MoneyFlow.Context;
+using MoneyFlow.DTOs;
+using MoneyFlow.Entities;
+using System.Transactions;
+
+namespace MoneyFlow.Managers
 {
-    public class TransactionManager
+    public class TransactionManager(AppDbContext _dbContext)
     {
-    }
+        public int New(TransactionDTO modelDTO)
+        {
+            var entity = new Transactions
+            {
+               
+                Date = modelDTO.Date,
+                TotalAmount = modelDTO.TotalAmount,
+                Comment = modelDTO.Comment,
+                Serviceid = modelDTO.Serviceid,
+                Userid = modelDTO.Userid
+
+            };
+            _dbContext.Transactions.Add(entity);
+            var rowsAffected = _dbContext.SaveChanges();
+
+            return rowsAffected;
+    }   }
 }
