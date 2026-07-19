afdd6cba5b5e9ca095553931462894fc747ca087
Cristopher MR <cristophermr04@gmail.com>
2026-07-18 21:00:02 -0400

    feat: Creacion del controlador y vista de transaction

---
+++ Context/TransactionController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoneyFlow.Managers;
+
+namespace MoneyFlow.Context
+{
+    public class TransactionController(ServiceManager _serviceManager, TransactionManager _transactionManager) : Controller
+    {
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
